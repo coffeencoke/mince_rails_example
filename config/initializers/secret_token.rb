09834db7b5effe1165661b4c603b98cc045e1682
Jason & Matt (Asynchrony) <dev@asolutions.com>
2012-03-02 15:35:17 -0600

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuitarShop::Application.config.secret_token = '565b2f400d272379040d1d7d087517f5bb8b3dcd97d175d2ab1b5a6fb3c5280c7b5cb8f9321aad4756f7ffbc568973a8b04943a7d276460bb120aa516b5f06a0'
